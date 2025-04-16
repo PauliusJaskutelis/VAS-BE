7bd7fd3f6b03c1a929eaae66810354f8e64ff8f7
PauliusJaskutelis <pjaskutelis@gmail.com>
2025-04-17 00:05:57 +0300

    image classification with selected model

--- src/main/java/com/fashiontrunk/fashiontrunkapi/Controllers/ImageController.java
+++ src/main/java/com/fashiontrunk/fashiontrunkapi/Controllers/ImageController.java
@@ -1,26 +1,38 @@
 package com.fashiontrunk.fashiontrunkapi.Controllers;
 
+import com.fashiontrunk.fashiontrunkapi.Models.ModelEntity;
 import com.fashiontrunk.fashiontrunkapi.Services.ClasificationService;
+import com.fashiontrunk.fashiontrunkapi.Services.ModelService;
 import com.fashiontrunk.fashiontrunkapi.Util.ImageValidation;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.*;
 
 @RestController
 @RequestMapping("/image")
 public class ImageController {
 
+    private final ModelService modelService;
+    ClasificationService classificationService = new ClasificationService();
+
+    @Autowired
+    public ImageController(ModelService modelService) {
+        this.modelService = modelService;
+    }
+
     @PostMapping
     public ResponseEntity<?> uploadImage(
             @RequestParam("files") MultipartFile[] files,
             @RequestParam(name = "prediction_count", defaultValue = "5") int predictionCount,
             @RequestParam(name = "confidence_threshold", defaultValue = "0.1") double confidenceThreshold
-    ) throws IOException {
+    ) {
         List<Object> results = new ArrayList<>();
 
         if (!ImageValidation.isValidImageBundle(files)) {
@@ -33,7 +45,7 @@ public class ImageController {
                 Map<String, Object> wrapped = new HashMap<>();
                 String result;
 
-                result = ClasificationService.sendToClassifier(
+                result = classificationService.sendToClassifier(
                         file, predictionCount, confidenceThreshold
                 );
 
@@ -48,5 +60,47 @@ public class ImageController {
         System.out.println(results);
         return ResponseEntity.ok(results);
 
+    }
+    @PostMapping("/classify-with-model/{id}")
+    public ResponseEntity<?> classifyWithModel(
+            @PathVariable UUID id,
+            @RequestParam("files") MultipartFile[] images,
+            @RequestParam("model_name") String modelName,
+            @RequestParam("prediction_count") int predictionCount,
+            @RequestParam("confidence_threshold") double confidenceThreshold
+    ) {
+        List<Object> results = new ArrayList<>();
+
+        if (!ImageValidation.isValidImageBundle(images)) {
+            return new ResponseEntity<>("File Not Accepted", HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+        }
+
+        Optional<ModelEntity> model = modelService.getModelById(id);
+        if (model.isEmpty()) {
+            return new ResponseEntity<>("Model not Found", HttpStatus.NOT_FOUND);
+        }
+        if(!model.isPresent())
+            System.out.println("WHAT THE FUCK");
+        for (MultipartFile image : images) {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                Map<String, Object> wrapped = new HashMap<>();
+                String result;
+
+                result = classificationService.classifyWithModel( id, model.get().getStoragePath(),
+                        image, predictionCount, confidenceThreshold
+                );
+                System.out.println(" " + result + " ");
+                wrapped.put("filename", image.getOriginalFilename());
+                wrapped.put("results", mapper.readValue(result, Map.class).get("results"));
+                results.add(wrapped);
+            } catch (Exception e) {
+                System.out.println(LocalDateTime.now() + " Error processing Image: " + image.getOriginalFilename());
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body("Error processing file: " + image.getOriginalFilename());
+            }
+        }
+
+        return ResponseEntity.ok(results);
     }
 }
--- src/main/java/com/fashiontrunk/fashiontrunkapi/Services/ClasificationService.java
+++ src/main/java/com/fashiontrunk/fashiontrunkapi/Services/ClasificationService.java
@@ -1,20 +1,52 @@
 package com.fashiontrunk.fashiontrunkapi.Services;
 
+import com.fashiontrunk.fashiontrunkapi.Util.ModelStorage;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
 
-public class ClasificationService {
-    public static String sendToClassifier(MultipartFile file, int predictionCount, double confidenceThreshold) throws IOException {
+@Service
+public class ClasificationService extends ContainerServiceBase {
+
+    private static final String CONTAINER_NAME = "custom-model-tf";
+    private static final String IMAGE_NAME = "custom-model-tf";
+    private static final int PORT = 5000;
+
+    @Override
+    protected String getContainerName(){ return CONTAINER_NAME; }
+
+    @Override
+    protected String getImageName(){ return IMAGE_NAME; }
+
+    @Override
+    protected int getPort(){ return PORT; }
+
+    @Override
+    protected String getHealthCheckUrl() { return "http://localhost:" + getPort() + "/docs"; }
+
+    public String sendToClassifier(
+            MultipartFile file,
+            int predictionCount,
+            double confidenceThreshold
+    ) throws IOException, InterruptedException {
+
+        startContainer();
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
@@ -29,7 +61,7 @@ public class ClasificationService {
         HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
         UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
-                .fromHttpUrl("http://localhost:5000/classify")
+                .fromHttpUrl("http://localhost:" + PORT + "/classify")
                 .queryParam("prediction_count", predictionCount)
                 .queryParam("confidence_threshold", confidenceThreshold);
         String url = uriComponentsBuilder.toUriString();
@@ -39,6 +71,64 @@ public class ClasificationService {
                 url, requestEntity, String.class
         );
 
+        return response.getBody();
+    }
+
+    public String classifyWithModel(
+            UUID modelId,
+            String storagePath,
+            MultipartFile image,
+            int predictionCount,
+            double confidenceThreshold
+    ) throws IOException, InterruptedException {
+
+        File modelFile;
+        MultiValueMap<String, Object> modelRequest = new LinkedMultiValueMap<>();
+        MultiValueMap<String, Object> imageRequest = new LinkedMultiValueMap<>();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        System.out.println("Point minus one");
+        startContainer();
+        System.out.println("Point zero");
+        boolean hasModel = false;
+        try {
+            ResponseEntity<Map> response = restTemplate.getForEntity("http://localhost:" + PORT + "/has-model", Map.class);
+            hasModel = Boolean.TRUE.equals(response.getBody().get("loaded"));
+        } catch (Exception e) {
+            System.out.println(LocalDateTime.now() + "Failed to Check model status");
+            throw new IOException("Failed to check model status", e);
+        }
+        System.out.println("Point half of one");
+        // 📤 Only upload the model if needed
+        if (!hasModel) {
+
+            modelFile = ModelStorage.getModelFile(modelId, storagePath);
+            if(!modelFile.exists()){
+                System.out.println("Model was not found" + modelFile.getAbsolutePath());
+                throw new IOException("Model was not found" + modelFile.getAbsolutePath());
+            }
+            try {
+                modelRequest.add("file", new FileSystemResource(modelFile));
+                restTemplate.postForEntity("http://localhost:" + PORT + "/upload-model", new HttpEntity<>(modelRequest, headers), String.class);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        System.out.println("Point one");
+        ByteArrayResource imageResource = new ByteArrayResource(image.getBytes()) {
+            @Override public String getFilename() { return image.getOriginalFilename(); }
+        };
+        System.out.println("Point two");
+        imageRequest.add("file", imageResource);
+
+        String classifyUrl = UriComponentsBuilder.fromHttpUrl("http://localhost:" + PORT + "/classify")
+                .queryParam("prediction_count", predictionCount)
+                .queryParam("confidence_threshold", confidenceThreshold)
+                .toUriString();
+        System.out.println("Paoint tree");
+        ResponseEntity<String> response = restTemplate.postForEntity(classifyUrl, new HttpEntity<>(imageRequest, headers), String.class);
+        System.out.println("Point four");
         return response.getBody();
     }
 }
